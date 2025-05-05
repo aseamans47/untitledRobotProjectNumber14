d8641bd65b931c240bab1d08f7f9844a0a5a2adc
aseamans47 <aseamans47@gmail.com>
2025-05-04 22:19:46 -0400

    motor goes brrrrt. motor works with the board now.

--- robot_motors/src/main.rs
+++ robot_motors/src/main.rs
@@ -3,50 +3,35 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::gpio::{Level, Output, OutputType, Speed };
+use embassy_stm32::time::khz;
+use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
+
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
 
-    let mut led1 = Output::new(p.PD12, Level::High, Speed::Low);
-    let mut led2 = Output::new(p.PD13, Level::High, Speed::Low);
-    let mut led3 = Output::new(p.PD14, Level::High, Speed::Low);
-    let mut led4 = Output::new(p.PD15, Level::High, Speed::Low);
+    info!("Hello World!");
+    
+    let ch1_pin = PwmPin::new_ch1(p.PB6, OutputType::PushPull);
+    let mut pwm = SimplePwm::new(p.TIM4, Some(ch1_pin), None, None, None, khz(20), Default::default());
+
+    let mut dir = Output::new(p.PB7, Level::Low, Speed::Low);
+
+    let mut ch1 = pwm.ch1();
+    ch1.enable();
 
     loop {
-        info!("led1 high");
-        led1.set_high();
+        ch1.set_duty_cycle_fully_off();
         Timer::after_millis(300).await;
-
-        info!("led1 low");
-        led1.set_low();
+        ch1.set_duty_cycle_fraction(1, 4);
         Timer::after_millis(300).await;
-
-        info!("led2 high");
-        led2.set_high();
+        ch1.set_duty_cycle_fraction(1, 2);
         Timer::after_millis(300).await;
-
-        info!("led2 low");
-        led2.set_low();
-        Timer::after_millis(300).await;
-
-        info!("led3 high");
-        led3.set_high();
-        Timer::after_millis(300).await;
-
-        info!("led3 low");
-        led3.set_low();
-        Timer::after_millis(300).await;
-
-        info!("led4 high");
-        led4.set_high();
-        Timer::after_millis(300).await;
-        info!("led4 low");
-        led4.set_low();
+        ch1.set_duty_cycle(ch1.max_duty_cycle() - 1);
         Timer::after_millis(300).await;
 
 
