5bc96293f9cb334aa77675144831a9da1dbec0de
aseamans47 <aseamans47@gmail.com>
2025-05-11 14:31:53 -0400

    wrote a program that moves the motor using the blue pill for a second. Next is using pwm for control

--- bluepill_motors/src/main.rs
+++ bluepill_motors/src/main.rs
@@ -12,15 +12,18 @@ async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
-    let mut led = Output::new(p.PC13, Level::High, Speed::Low);
+    let mut motor_in1 = Output::new(p.PC13, Level::High, Speed::Low);
+    let mut motor_in2 = Output::new(p.PC14, Level::High, Speed::Low);
 
     loop {
-        info!("high");
-        led.set_high();
-        Timer::after_millis(300).await;
+        info!("fwd");
+        motor_in1.set_level(Level::Low);
+        motor_in2.set_high();
+        Timer::after_millis(1000).await;
+        return;
 
-        info!("low");
-        led.set_low();
-        Timer::after_millis(300).await;
+        //info!("low");
+        //motor_in1.set_low();
+        //Timer::after_millis(300).await;
     }
 }
